9a1181c4bda45ad39ea8ce80eb2c313c3c556f47
Omoti <guti.tomtom@gmail.com>
2022-08-11 15:30:00 +0900

    Initial Commit

---
+++ SealProgrammingMobile/SealProgrammingMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SealProgrammingMobileApp.swift
+//  SealProgrammingMobile
+//
+//  Created by 坂口智典 on 2022/08/11.
+//
+
+import SwiftUI
+
+@main
+struct SealProgrammingMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
