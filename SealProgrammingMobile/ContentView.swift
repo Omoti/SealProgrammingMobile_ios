c072988ecf258a5a761c8af0dfb516d91e9a1481
Omoti <guti.tomtom@gmail.com>
2022-08-12 13:59:47 +0900

    add:ImagePicker, Camera

--- SealProgrammingMobile/ContentView.swift
+++ SealProgrammingMobile/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showingImagePicker = false
+    @State var showingCameraPicker = false
+    @State var image: UIImage?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+            }
+            Text("Image")
+                .onTapGesture {
+                    showingImagePicker.toggle()
+                }
+            Text("Camera")
+                .onTapGesture {
+                    showingCameraPicker.toggle()
+                }
+        }.sheet(isPresented:$showingImagePicker) {
+            ImagePickerView(image: $image, sourceType: .library)
+        }.sheet(isPresented:$showingCameraPicker) {
+            ImagePickerView(image: $image, sourceType: .camera)
+        }
     }
 }
 
