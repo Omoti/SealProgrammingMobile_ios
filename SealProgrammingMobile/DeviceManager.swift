b3e5dd9aa338eb7d4d41a9aa13ab417cfc1f8e16
Omoti <guti.tomtom@gmail.com>
2022-09-10 06:22:03 +0900

    feat:見つかったペリフェラルをリスト化

--- SealProgrammingMobile/DeviceManager.swift
+++ SealProgrammingMobile/DeviceManager.swift
@@ -2,10 +2,12 @@ import CoreBluetooth
 
 class DeviceManager: NSObject, ObservableObject, CBCentralManagerDelegate {
     @Published var isSearching: Bool = false
+    @Published var foundPeripherals: [Peripheral] = []
     
     private var centralManager: CBCentralManager!
     
     func startScan() {
+        foundPeripherals.removeAll()
         centralManager = CBCentralManager(delegate: self, queue: nil)
         isSearching = true
     }
@@ -28,7 +30,35 @@ class DeviceManager: NSObject, ObservableObject, CBCentralManagerDelegate {
     }
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber){
-        print(peripheral)
+    
+        if RSSI.intValue >= 0 { return }
+        
+        let peripheralName = advertisementData[CBAdvertisementDataLocalNameKey] as? String ?? nil
+        var _name = "NoName"
+        
+        if peripheralName != nil {
+            _name = String(peripheralName!)
+        } else if peripheral.name != nil {
+            _name = String(peripheral.name!)
+        }
+        
+        let foundPeripheral: Peripheral = Peripheral(name: _name,
+                                                     rssi: RSSI.intValue,
+                                                     uuid: peripheral.identifier.uuidString,
+                                                     discoverCount: 0)
+        
+        // 50回に一回検出
+        if let index = foundPeripherals.firstIndex(where: { $0.uuid == peripheral.identifier.uuidString }) {
+            if foundPeripherals[index].discoverCount % 50 == 0 {
+                foundPeripherals[index].name = _name
+                foundPeripherals[index].rssi = RSSI.intValue
+                foundPeripherals[index].discoverCount += 1
+            } else {
+                foundPeripherals[index].discoverCount += 1
+            }
+        } else {
+            print(peripheral)
+            foundPeripherals.append(foundPeripheral)
+        }
     }
 }
-
---
+++ SealProgrammingMobile/Peripheral.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct Peripheral{
+    var name: String = ""
+    var rssi: Int = 0
+    var uuid: String = ""
+    var discoverCount: Int = 0
+}
