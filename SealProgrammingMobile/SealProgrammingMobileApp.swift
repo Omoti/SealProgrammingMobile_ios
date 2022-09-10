362676d7450858c9105ade16c4bd5db71897cf40
Omoti <guti.tomtom@gmail.com>
2022-09-11 08:50:51 +0900

    add:NavigationBar

--- SealProgrammingMobile/SealProgrammingMobileApp.swift
+++ SealProgrammingMobile/SealProgrammingMobileApp.swift
@@ -2,6 +2,16 @@ import SwiftUI
 
 @main
 struct SealProgrammingMobileApp: App {
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(named: "PrimaryColor")
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
     var body: some Scene {
         WindowGroup {
             MainScreenView()
--- SealProgrammingMobile/screens/main/MainScreenView.swift
+++ SealProgrammingMobile/screens/main/MainScreenView.swift
@@ -11,67 +11,71 @@ struct MainScreenView: View{
     @State var detectionResult: DetectionResult?
     
     var body:some View{
-        VStack(alignment: .center) {
-            ZStack{
-                if let image = pickedImage {
-                    Image(uiImage: image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .background(Color.white)
-                        .frame(maxHeight: .infinity)
-                }else{
-                    VStack{
-                        Spacer()
-                        Text("シールプログラミングへようこそ")
-                        Spacer()
-                    }.background(Color.white)
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        NavigationView{
+            VStack(alignment: .center) {
+                ZStack{
+                    if let image = pickedImage {
+                        Image(uiImage: image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .background(Color.white)
+                            .frame(maxHeight: .infinity)
+                    }else{
+                        VStack{
+                            Spacer()
+                            Text("シールプログラミングへようこそ")
+                            Spacer()
+                        }.background(Color.white)
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                    }
+                    if let detectionResult = detectionResult {
+                        DetectionResultView(detections: detectionResult.detections, imageSize: pickedImage!.size)
+                    }
+                }.aspectRatio(3/4, contentMode: ContentMode.fit)
+                    .background(Color.white)
+                Spacer()
+                HStack(alignment: .center){
+                    Spacer()
+                    CircleButton(
+                        image: Image(systemName: "camera.fill"),
+                        label: "とる",
+                        color: Color.blue,
+                        action: {
+                            // showingCameraPicker.toggle()
+                            showingImagePicker.toggle()
+                        }
+                    )
+                    Spacer()
+                    CircleButton(
+                        image: Image("BluetoothIconDefault"),
+                        label: "つなぐ",
+                        color: Color.blue,
+                        action: {
+                            showingDeviceScanView = true
+                        }
+                    )
+                    Spacer()
+                    CircleButton(
+                        image:Image(systemName: "car.fill"),
+                        label: "おくる",
+                        color: Color.orange,
+                        action: {
+                            // TODO : プログラムをおくる
+                        }
+                    )
+                    Spacer()
                 }
-                if let detectionResult = detectionResult {
-                    DetectionResultView(detections: detectionResult.detections, imageSize: pickedImage!.size)
-                }
-            }.aspectRatio(3/4, contentMode: ContentMode.fit)
-                .background(Color.white)
-            Spacer()
-            HStack(alignment: .center){
-                Spacer()
-                CircleButton(
-                    image: Image(systemName: "camera.fill"),
-                    label: "とる",
-                    color: Color.blue,
-                    action: {
-                        // showingCameraPicker.toggle()
-                        showingImagePicker.toggle()
-                    }
-                )
-                Spacer()
-                CircleButton(
-                    image: Image("BluetoothIconDefault"),
-                    label: "つなぐ",
-                    color: Color.blue,
-                    action: {
-                        showingDeviceScanView = true
-                    }
-                )
-                Spacer()
-                CircleButton(
-                    image:Image(systemName: "car.fill"),
-                    label: "おくる",
-                    color: Color.orange,
-                    action: {
-                        // TODO : プログラムをおくる
-                    }
-                )
-                Spacer()
-            }
-        }.sheet(isPresented:$showingImagePicker) {
-            ImagePickerView(image: $pickedImage, sourceType: .library)
-        }.sheet(isPresented:$showingCameraPicker) {
-            ImagePickerView(image: $pickedImage, sourceType: .camera)
-        }.sheet(isPresented: $showingDeviceScanView) {
-            DeviceScanView()
-        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-            .background(Color("ControlBackgroundColor"))
+            }.sheet(isPresented:$showingImagePicker) {
+                ImagePickerView(image: $pickedImage, sourceType: .library)
+            }.sheet(isPresented:$showingCameraPicker) {
+                ImagePickerView(image: $pickedImage, sourceType: .camera)
+            }.sheet(isPresented: $showingDeviceScanView) {
+                DeviceScanView()
+            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                .background(Color("ControlBackgroundColor"))
+                .navigationBarTitle("シールプログラミング")
+                .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
