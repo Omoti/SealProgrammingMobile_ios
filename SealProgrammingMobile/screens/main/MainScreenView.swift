10a1d9a3e0932a20b2e69305f77b77522ce4fa53
Omoti <guti.tomtom@gmail.com>
2022-09-10 20:17:13 +0900

    feat:MainScreenレイアウト

--- SealProgrammingMobile/screens/main/MainScreenView.swift
+++ SealProgrammingMobile/screens/main/MainScreenView.swift
@@ -11,7 +11,7 @@ struct MainScreenView: View{
     @State var detectionResult: DetectionResult?
     
     var body:some View{
-        VStack() {
+        VStack(alignment: .center) {
             ZStack{
                 if let image = pickedImage {
                     Image(uiImage: image)
@@ -20,40 +20,52 @@ struct MainScreenView: View{
                         .background(Color.gray)
                         .frame(maxHeight: .infinity)
                 }else{
-                    Spacer()
+                    Spacer().background(Color.white)
                 }
                 if let detectionResult = detectionResult {
                     DetectionResultView(detections: detectionResult.detections, imageSize: pickedImage!.size)
                 }
-            }
-            Text("Image")
-                .onTapGesture {
-                    showingImagePicker.toggle()
-                }
-            Spacer().frame(height: 10)
-            Text("Camera")
-                .onTapGesture {
-                    showingCameraPicker.toggle()
-                }
-            Spacer().frame(height: 10)
-            Text("Detect")
-                .onTapGesture {
-                    if(pickedImage != nil) {
-                        detectionResult = SealDetector.detect(image: pickedImage!)
+            }.aspectRatio(3/4, contentMode: ContentMode.fit)
+                .background(Color.white)
+            Spacer()
+            HStack(alignment: .center){
+                Spacer()
+                Button(
+                    action: {
+                        showingCameraPicker.toggle()
+                    },
+                    label: {
+                        Text("カメラ")
                     }
-                }
-            Spacer().frame(height: 10)
-            Text("Connect")
-                .onTapGesture {
-                    showingDeviceScanView = true
-                }
+                )
+                Spacer()
+                Button(
+                    action: {
+                        showingDeviceScanView = true
+                    },
+                    label: {
+                        Text("つなぐ")
+                    }
+                )
+                Spacer()
+                Button(
+                    action: {
+                        // TODO : コマンドを送信
+                    },
+                    label: {
+                        Text("おくる")
+                    }
+                )
+                Spacer()
+            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, alignment: .center)
+                .padding(20)
         }.sheet(isPresented:$showingImagePicker) {
             ImagePickerView(image: $pickedImage, sourceType: .library)
         }.sheet(isPresented:$showingCameraPicker) {
             ImagePickerView(image: $pickedImage, sourceType: .camera)
         }.sheet(isPresented: $showingDeviceScanView) {
             DeviceScanView()
-        }
+        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
     }
 }
 
