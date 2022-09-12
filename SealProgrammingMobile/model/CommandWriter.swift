470edf46ade382a40f057da8d368fe238f67e96b
Omoti <guti.tomtom@gmail.com>
2022-09-12 09:15:50 +0900

    feat:コマンド送信フォーマット

--- SealProgrammingMobile/model/CommandWriter.swift
+++ SealProgrammingMobile/model/CommandWriter.swift
@@ -2,7 +2,29 @@ import CoreBluetooth
 
 struct CommandWriter{
     static func write(peripheral: CBPeripheral, characteristic: CBCharacteristic, commands: String){
-        let data = commands.data(using: .utf8)!
+        // 転送中LED点灯
+        writeStrring(peripheral: peripheral, characteristic: characteristic, command: SerialCommand.ledRed100)
+        
+        // クリア
+        writeStrring(peripheral: peripheral, characteristic: characteristic, command: SerialCommand.clear)
+        
+        // 点滅
+        writeStrring(peripheral: peripheral, characteristic: characteristic, command: SerialCommand.blink)
+        
+        // 位置文字ずつ送信
+        for command in commands {
+            writeStrring(peripheral: peripheral, characteristic: characteristic, command: String(command))
+        }
+        
+        // ゴール
+        writeStrring(peripheral: peripheral, characteristic: characteristic, command: SerialCommand.goal)
+        
+        // 転送中LED消灯
+        writeStrring(peripheral: peripheral, characteristic: characteristic, command: SerialCommand.ledRed0)
+    }
+    
+    private static func writeStrring(peripheral: CBPeripheral, characteristic: CBCharacteristic, command: String){
+        let data = command.data(using: .utf8)!
         peripheral.writeValue(data , for: characteristic, type: .withResponse)
     }
 }
