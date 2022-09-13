1dcb51bf6c95d5e77fc337e9c018e4a522d9b01c
Omoti <guti.tomtom@gmail.com>
2022-09-13 22:41:42 +0900

    feat:プレビューアスペクト比指定

--- SealProgrammingMobile/screens/camera/CaptureScreenView.swift
+++ SealProgrammingMobile/screens/camera/CaptureScreenView.swift
@@ -1,16 +1,17 @@
 import SwiftUI
 
-
 struct CaptureScreenView: View {
     @EnvironmentObject private var deviceManager :DeviceManager
     @StateObject private var camera = CamearaManager() // 再描画しても状態を保持
     
     var body: some View {
-        ZStack{
-            PreviewView(camera: camera)
-                .ignoresSafeArea(.all, edges: .all)
-        }.onAppear(){
-            camera.setupCaptureSession()
+        VStack{
+            ZStack{
+                PreviewView(camera: camera, aspectRatio: 3/4)
+            }.onAppear(){
+                camera.setupCaptureSession()
+            }.aspectRatio(3/4, contentMode: ContentMode.fit)
+            Spacer()
         }
     }
 }
--- SealProgrammingMobile/screens/camera/PreviewView.swift
+++ SealProgrammingMobile/screens/camera/PreviewView.swift
@@ -4,9 +4,12 @@ import SwiftUI
 
 struct PreviewView: UIViewRepresentable {
     @ObservedObject var camera: CamearaManager
+    var aspectRatio: Double
     
     func makeUIView(context: Context) -> UIView {
-        let view = UIView(frame: UIScreen.main.bounds)
+        let screen = UIScreen.main.bounds
+        let frame = CGRect(x: screen.minX, y: screen.minY, width: screen.width, height: screen.width / aspectRatio)
+        let view = UIView(frame: frame)
         camera.preview = AVCaptureVideoPreviewLayer(session: camera.captureSession)
         camera.preview.frame = view.frame
         
@@ -18,5 +21,8 @@ struct PreviewView: UIViewRepresentable {
         return view
     }
 
-    func updateUIView(_ uiView: UIView, context: Context) {}
+    func updateUIView(_ uiView: UIView, context: Context) {
+        camera.preview.frame = uiView.frame
+        print(camera.preview.frame)
+    }
 }
