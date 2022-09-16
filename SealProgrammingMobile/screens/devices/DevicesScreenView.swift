5a9f8bc8fc3582ee55cca89894e77e6a6be311a6
Omoti <guti.tomtom@gmail.com>
2022-09-17 08:09:31 +0900

    add:デバイス画面ヘッダ

--- SealProgrammingMobile/screens/devices/DevicesScreenView.swift
+++ SealProgrammingMobile/screens/devices/DevicesScreenView.swift
@@ -1,25 +1,39 @@
 import SwiftUI
 
 struct DeviceScanView: View {
+    @Environment(\.dismiss) private var dismiss
     @EnvironmentObject private var deviceModel :DeviceModel
     
     var onConnect: () -> Void
     
     var body: some View {
-        List(){
-            ForEach(deviceModel.foundPeripherals, id: \.self) { peripheral in
-                FoundDeviceItem(
-                    name:peripheral.name,
-                    action: {
-                        deviceModel.connect(peripheral: peripheral)
-                        onConnect()
-                    }
-                )
+        VStack{
+            HStack(alignment: .bottom){
+                Spacer()
+                CloseButton(action: {
+                    close()
+                })
+            }.padding(EdgeInsets(top: 10, leading: 10, bottom: 3, trailing: 10))
+                .background(Color("PrimaryColor"))
+            List(){
+                ForEach(deviceModel.foundPeripherals, id: \.self) { peripheral in
+                    FoundDeviceItem(
+                        name:peripheral.name,
+                        action: {
+                            deviceModel.connect(peripheral: peripheral)
+                            onConnect()
+                        }
+                    )
+                }
             }
         }.onAppear(){
             deviceModel.startScan()
         }.onDisappear(){
             deviceModel.stopScan()
-        }
+        }.background(Color("PrimaryColor"))
+    }
+    
+    func close(){
+        dismiss()
     }
 }
