d15cd48ead08a361a972cebcfe329a6c689fad97
Omoti <guti.tomtom@gmail.com>
2022-09-18 09:26:49 +0900

    feat:10件ごとに区切り線

--- SealProgrammingMobile/screens/seals/SealsScreenView.swift
+++ SealProgrammingMobile/screens/seals/SealsScreenView.swift
@@ -17,6 +17,7 @@ struct SealsScreenView : View {
                 List(){
                     ForEach(seals.indices.reversed(), id: \.self) { index in
                         SealItem(index: index + 1, seal: seals[index])
+                            .listRowSeparatorTint(index % 10 == 0 ? .black : .clear)
                     }.onAppear(){
                         reader.scrollTo(0)
                     }
