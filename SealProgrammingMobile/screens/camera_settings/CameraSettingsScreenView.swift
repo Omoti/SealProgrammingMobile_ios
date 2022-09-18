edeeea15b70d82019e4b17ebbc3651dbcbb0a246
Omoti <guti.tomtom@gmail.com>
2022-09-18 17:35:28 +0900

    feat:設定画面にしきい値追加

--- SealProgrammingMobile/screens/camera_settings/CameraSettingsScreenView.swift
+++ SealProgrammingMobile/screens/camera_settings/CameraSettingsScreenView.swift
@@ -7,7 +7,18 @@ struct CameraSettingsScreenView : View{
         VStack{
             HStack{
                 Form {
-                    Section(){
+                    Section{
+                        VStack{
+                            Text("写真からシールを認識する際のしきい値を設定します。\nシールが認識されにくいときは、しきい値を下げてください。\nシール以外のものを誤って認識するときは、しきい値を上げてください。")
+                                .font(.caption)
+                                .fixedSize(horizontal: false, vertical: true)
+                            Slider(value: $settings.threshold, in: 0...1, step: 0.1)
+                            Text("\(settings.threshold * 100, specifier: "%.0f")%")
+                        }.padding(10)
+                    } header : {
+                        Text("シール検出設定")
+                    }
+                    Section{
                         Toggle(isOn: $settings.showScore) {
                             Text("検出結果の信頼度(%)を表示")
                         }
