87365f5363e220007fc3c427991313991fcc656a
Omoti <guti.tomtom@gmail.com>
2022-09-18 17:15:35 +0900

    feat:CameraSettingsにthreshold追加

--- SealProgrammingMobile/model/CameraSettings.swift
+++ SealProgrammingMobile/model/CameraSettings.swift
@@ -2,16 +2,24 @@ import Foundation
 
 class CameraSettings : ObservableObject{
     let userDefaults = UserDefaults.standard
-        
+    
     @Published var showScore: Bool {
-            didSet {
-                userDefaults.set(showScore, forKey: "show_score")
-            }
+        didSet {
+            userDefaults.set(showScore, forKey: "show_score")
         }
-
+    }
+    
+    @Published var threshold: Float {
+        didSet {
+            userDefaults.set(threshold, forKey: "threshold")
+        }
+    }
+    
     init() {
-        userDefaults.register(defaults: ["show_score" : false])
+        userDefaults.register(defaults: ["show_score" : false,
+                                         "threshold" : 0.4])
         
         showScore = userDefaults.bool(forKey: "show_score")
+        threshold = userDefaults.float(forKey: "threshold")
     }
 }
